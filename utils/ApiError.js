4b278e2529df78fbec3642d38693f1cbfc873fd3
ipiyumii <sendtopiyumi@gmail.com>
2026-09-24 14:09:48 +0530

    Revert "feat: add ETag support for conditional GET requests"
    This reverts commit e7a12dc461dc97d4af951160a5e1cbf30e32e9c2.

--- utils/ApiError.js
+++ utils/ApiError.js
@@ -31,10 +31,6 @@ class ApiError extends Error {
   static conflict(code, message, details = []) {
     return new ApiError(409, code, message, details);
   }
-
-  static preconditionFailed(code = 'PRECONDITION_FAILED', message = 'Precondition failed.') {
-    return new ApiError(412, code, message);
-  }
 }
 
 module.exports = ApiError;
